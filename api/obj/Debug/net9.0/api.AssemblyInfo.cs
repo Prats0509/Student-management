0fb2aa94db65794a896f4127fdc0c0127d261566
Pratiksha Dutta <hsbaruah1997@gmail.com>
2025-03-11 11:37:41 -0400

    Added mvc

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d294fe0cc634bf14b9c083d53ffb04d6ea4f8fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29026c6c05f41c7d7bcf2d435aba2d3d951388ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
